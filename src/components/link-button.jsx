d503b01560dd2de323c0569b42f45e15dd432866
Artem <artem@MacBook-Pro.local>
2025-04-15 19:45:40 +0300

    fix phone and btn

--- src/components/link-button.jsx
+++ src/components/link-button.jsx
@@ -4,10 +4,10 @@ export default function LinkButton({ text = '', href = '#', type = 1, className 
 	const baseStyles = `inline-block font-normal px-7 py-2 rounded-full transition-colors duration-300`
 
 	const typeStyles = {
-		1: 'bg-[--main-color] text-white hover:bg-[--second-color]',
-		2: 'bg-transparent text-[--second-color] border border-[--main-color] hover:bg-[--second-color] hover:text-[--main-color]',
-		3: 'bg-white text-[--main-color] hover:bg-[--second-color] hover:text-[--white-bc] h-auto',
-		4: 'bg-transparent text-white border border-white hover:bg-white hover:text-[--background-color]',
+		1: 'hover:opacity-100 bg-[--main-color] text-white hover:bg-[--second-color]',
+		2: 'hover:opacity-100 bg-transparent text-[--second-color] border border-[--main-color] hover:bg-[--second-color] hover:text-[--main-color]',
+		3: 'hover:opacity-100 bg-white text-[--main-color] hover:bg-[--second-color] hover:text-[--white-bc] h-auto',
+		4: 'hover:opacity-100 bg-transparent text-white border border-white hover:bg-white hover:text-[--background-color]',
 	}
 
 	const classes = `${baseStyles} ${typeStyles[type] || typeStyles[1]} ${className}`
